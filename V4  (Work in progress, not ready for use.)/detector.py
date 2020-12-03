b14da3f0cd1ddca9fdd0c1887bda2a012aa486f6
Ruud14 <ruudbrouwers@hotmail.com>
2020-12-03 20:33:24 +0100

    Cleaned up V4

--- V4  (Work in progress, not ready for use.)/detector.py
+++ V4  (Work in progress, not ready for use.)/detector.py
@@ -3,92 +3,108 @@ import threading
 import numpy as np
 import urllib.request
 import json
-import socket
 
-#TODO: change to localhost!
-stream_url = "http://192.168.178.207:8000/stream.mjpg"
-
-# Getting private data from 'data.json'
-file = open('data.json')
-stored_data = json.loads(file.read())
-username = stored_data["username"]
-password = stored_data["password"]
-file.close()
-current_frame = None
-current_lowres_frame = None
-motion_sensitivity = 50 # Higher number = less detection
-
-def login():
-    try:
-        # create a password manager
-        password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-        password_mgr.add_password(None, stream_url, username, password)
-        handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-        opener = urllib.request.build_opener(handler)
-        opener.open(stream_url)
-        urllib.request.install_opener(opener)
-        print("Camera login succeeded.")
-        return True
-    except:
-        print("Camera login failed.")
-        return False
-
-# Receives the live video feed from the camera.
-def recv_stream():
-    global current_frame, current_lowres_frame
-    # start streaming
-    stream = urllib.request.urlopen(stream_url)
-    bytes = b''
-    while True:
-
-        bytes += stream.read(1024)
-        a = bytes.find(b'\xff\xd8')
-        b = bytes.find(b'\xff\xd9')
-        if a != -1 and b != -1:
-            jpg = bytes[a:b + 2]
-            bytes = bytes[b + 2:]
-            i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-            # Resize the low-res image for faster detection.
-            #TODO: make low res resolution changable.
-            lowres_i = cv2.resize(i, (640, 480), interpolation = cv2.INTER_AREA)
-            current_lowres_frame = lowres_i
-            current_frame = i
-            if cv2.waitKey(1) == 27:
-                break
-
-    print("Loop Broke")
-    exit(0)
+stream_url = "http://{}:8000/stream.mjpg"
 
 
-def detect_motion():
-    while True:
+# Class that handles logging into the camera.
+class Authentication:
+    def __init__(self, ip):
+        self.ip = ip
+        self.password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+        self.url = stream_url.format(ip)
+
+    # Log into the camera.
+    def authenticate(self, username, password):
         try:
-            current_lowres_frame.any()
-            break
-        except:
-            continue
+            self.password_mgr.add_password(None, self.url, username, password)
+            handler = urllib.request.HTTPBasicAuthHandler(self.password_mgr)
+            opener = urllib.request.build_opener(handler)
+            opener.open(self.url)
+            urllib.request.install_opener(opener)
+            print(f"{self.ip} login succeeded.")
+            return True
+        except Exception: #todo: Get right exception.
+            print("Camera login failed.")
+            return False
 
-    print("Detector started.")
 
-    start_frame = cv2.cvtColor(current_lowres_frame, cv2.COLOR_BGR2GRAY)
-    while True:
-        next_frame = cv2.cvtColor(current_lowres_frame, cv2.COLOR_BGR2GRAY)
+# Class that handles the detection of motion in the live camera feed.
+class Detector:
+    def __init__(self, ip, username, password, sensitivity, detection_resolution=(640, 480)):
+        self.ip = ip
+        self.credentials = (username, password)
+        # The sensitivity. Higher number = less detection.
+        self.motion_sensitivity = sensitivity
+        # Determines the resolution on which the motion is detected.
+        # Lower requires less processing power, but might be less precise.
+        self.detection_resolution = detection_resolution
+        # Create the current frame variables.
+        self._current_frame = None
+        self._current_low_res_frame = None
 
-        #TODO: Show frames for testing.
-        # cv2.imshow("Resized image", next_frame)
-        # cv2.waitKey(1)
+    # Starts the detector.
+    def start(self):
+        # Log into the camera.
+        authenticated = Authentication(self.ip).authenticate(*self.credentials)
+        # Start the detection of motion if the authentication succeeded.
+        if authenticated:
+            receive_thread = threading.Thread(target=self.receive_steam)
+            detect_thread = threading.Thread(target=self.detect_motion)
+            receive_thread.start()
+            detect_thread.start()
 
-        frame_difference = cv2.absdiff(next_frame, start_frame)
-        thresh = cv2.threshold(frame_difference, motion_sensitivity, 255, cv2.THRESH_BINARY)[1]
-        start_frame = next_frame
+    # Receives the live video feed from the camera.
+    def receive_steam(self):
+        # start streaming
+        stream = urllib.request.urlopen(stream_url.format(self.ip))
+        bytes = b''
+        while True:
+            bytes += stream.read(1024)
+            frame_start = bytes.find(b'\xff\xd8')
+            frame_end = bytes.find(b'\xff\xd9')
+            if frame_start != -1 and frame_end != -1:
+                jpg = bytes[frame_start:frame_end + 2]
+                bytes = bytes[frame_end + 2:]
+                frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                # Resize the low-res image for faster detection.
+                #TODO: make low res resolution changable.
+                low_res_frame = cv2.resize(frame, self.detection_resolution, interpolation=cv2.INTER_AREA)
+                self._current_low_res_frame = low_res_frame
+                self._current_frame = frame
 
-        if thresh.sum() > 100:
-            print("movement detected")
-        else:
-            pass
+    def detect_motion(self):
+        # Wait until the first frame is found. Start detecting afterwards.
+        while True:
+            try:
+                self._current_low_res_frame.any()
+                break
+            except Exception:
+                continue
+
+        print(f"Motion detection on {self.ip} started.")
+        # get the previous frame.
+        start_frame = cv2.cvtColor(self._current_low_res_frame, cv2.COLOR_BGR2GRAY)
+        while True:
+            # get the current frame.
+            next_frame = cv2.cvtColor(self._current_low_res_frame, cv2.COLOR_BGR2GRAY)
+
+            #TODO: Show frames for testing.
+            # cv2.imshow("Resized image", next_frame)
+            # cv2.waitKey(1)
+
+            # Calculate the difference between the current and previous frame.
+            frame_difference = cv2.absdiff(next_frame, start_frame)
+            thresh = cv2.threshold(frame_difference, self.motion_sensitivity, 255, cv2.THRESH_BINARY)[1]
+            start_frame = next_frame
+
+            # Start recording when the difference between the frames is too big.
+            if thresh.sum() > 100:
+                print(f"Movement detected on {self.ip}.")
 
 
 if __name__ == "__main__":
-    if login():
-        recv_thread = threading.Thread(target=recv_stream).start()
-        detect_thread = threading.Thread(target=detect_motion).start()
+    with open('config.json') as file:
+        stored_data = json.loads(file.read())
+        for camera in stored_data:
+            Detector(*list(camera.values())).start()
