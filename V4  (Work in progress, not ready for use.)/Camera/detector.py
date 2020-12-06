4d40c84dc5ec6d2245767f14b9d5de30ab041095
Ruud14 <ruudbrouwers@hotmail.com>
2020-12-06 16:29:35 +0100

    Reduced delay by always picking the last frame in the queue

--- V4  (Work in progress, not ready for use.)/Camera/detector.py
+++ V4  (Work in progress, not ready for use.)/Camera/detector.py
@@ -1,6 +1,5 @@
 import cv2
 import threading
-import numpy as np
 import urllib.request
 import json
 import subprocess
@@ -9,7 +8,6 @@ import datetime
 import time
 import socket
 
-stream_url = "http://{}:8000/stream.mjpg"
 config_file_path = '/home/pi/scripts/V4/config.json'
 
 
@@ -18,7 +16,7 @@ class Authentication:
     def __init__(self, ip):
         self.ip = ip
         self.password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-        self.url = stream_url.format(ip)
+        self.url = "http://{}:8000/stream.mjpg".format(ip)
 
     # Log into the camera.
     def authenticate(self, username, password):
@@ -31,7 +29,7 @@ class Authentication:
             print("{} login succeeded.".format(self.ip))
             return True
         except urllib.error.URLError:
-            print("Camera login failed.")
+            print("Camera login failed. You are probably using the wrong credentials, or the camera is not running.")
             return False
 
 
@@ -45,9 +43,10 @@ class Detector:
         # Determines the resolution on which the motion is detected.
         # Lower requires less processing power, but might be less precise.
         self.detection_resolution = detection_resolution
-        # Create the current frame variables.
-        self._current_frame = None
-        self._current_low_res_frame = None
+        # Create the video capture
+        self.cap = cv2.VideoCapture('http://{}:{}@localhost:8000/stream.mjpg'.format(*self.credentials))
+        # Reduce the buffer size.
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
         # Create the recorder
         self.recorder = Recorder(self.ip, self.credentials)
 
@@ -57,48 +56,27 @@ class Detector:
         authenticated = Authentication(self.ip).authenticate(*self.credentials)
         # Start the detection of motion if the authentication succeeded.
         if authenticated:
-            receive_thread = threading.Thread(target=self._receive_steam)
             detect_thread = threading.Thread(target=self._detect_motion)
-            receive_thread.start()
             detect_thread.start()
 
-    # Receives the live video feed from the camera.
-    def _receive_steam(self):
-        # start streaming
-        stream = urllib.request.urlopen(stream_url.format(self.ip))
-        bytes = b''
-        while True:
-            bytes += stream.read(1024)
-            frame_start = bytes.find(b'\xff\xd8')
-            frame_end = bytes.find(b'\xff\xd9')
-            if frame_start != -1 and frame_end != -1:
-                jpg = bytes[frame_start:frame_end + 2]
-                bytes = bytes[frame_end + 2:]
-                frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                # Resize the low-res image for faster detection.
-                #TODO: make low res resolution changable.
-                low_res_frame = cv2.resize(frame, self.detection_resolution, interpolation=cv2.INTER_AREA)
-                self._current_low_res_frame = low_res_frame
-                self._current_frame = frame
+    # Returns a resized version of the last frame from the live camera feed.
+    def _get_current_low_res_frame(self):
+        ret, frame = self.cap.read()
+        low_res_frame = cv2.resize(frame, self.detection_resolution, interpolation=cv2.INTER_AREA)
+        return low_res_frame
 
+    # Detects motion in the live video feed.
     def _detect_motion(self):
         blur = (5, 5)
-        # Wait until the first frame is found. Start detecting afterwards.
-        while True:
-            try:
-                self._current_low_res_frame.any()
-                break
-            except Exception:
-                continue
-
         print("Motion detection on {} started.".format(self.ip))
         # get the previous frame.
-        start_frame = cv2.cvtColor(self._current_low_res_frame, cv2.COLOR_BGR2GRAY)
+        start_frame = cv2.cvtColor(self._get_current_low_res_frame(), cv2.COLOR_BGR2GRAY)
         start_frame = cv2.GaussianBlur(start_frame, blur, 0)
         while True:
             # get the current frame.
-            next_frame = cv2.cvtColor(self._current_low_res_frame, cv2.COLOR_BGR2GRAY)
+            next_frame = cv2.cvtColor(self._get_current_low_res_frame(), cv2.COLOR_BGR2GRAY)
             next_frame = cv2.GaussianBlur(next_frame, blur, 0)
+
             # TODO: Show frames for testing.
             # cv2.imshow("Resized image", next_frame)
             # cv2.waitKey(1)
